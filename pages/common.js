44093a7221e816d9171bcf82d3fe04ada44a3741
LIWENDA888 <744223766@qq.com>
2025-10-30 14:35:30 +0800

    Add files via upload

--- pages/common.js
+++ pages/common.js
@@ -6,13 +6,15 @@
 const navData = [
     { label: '首页', url: 'https://www.zizao.top', openInNewTab: false },
 
-    { label: '产品', url: 'https://www.zizao.top/fonts', openInNewTab: false },
+    { label: '字体产品', url: 'https://www.zizao.top/fonts', openInNewTab: false },
     
-    { label: '授权', url: 'https://www.zizao.top/fonts/auth', openInNewTab: false },
+    { label: '字体授权', url: 'https://www.zizao.top/fonts/auth', openInNewTab: false },
+
+    { label: '设计导航', url: 'https://www.zizao.top/hao', openInNewTab: false },
+
+    { label: '设计资源', url: 'https://www.zizao.top/bao', openInNewTab: false },
 
     { label: '工具', url: '#', hasDropdown: true, subMenu: [
-        { label: '设计导航', url: 'https://www.zizao.top/hao', openInNewTab: true },
-        { label: '百宝库', url: 'https://www.zizao.top/bao', openInNewTab: false },
         { label: '字造超引', url: 'https://www.zizao.top/tools/chaoyin', openInNewTab: true },
         { label: '搜书法风格', url: 'https://www.zizao.top/tools/shufa', openInNewTab: true },
         { label: '文本编辑', url: 'https://www.zizao.top/tools/txt', openInNewTab: true },
@@ -21,7 +23,7 @@ const navData = [
         { label: '文本去重', url: 'https://www.zizao.top/tools/quchong', openInNewTab: true }
     ]},
 
-    { label: '关于', url: 'https://www.zizao.top/fonts/about', openInNewTab: false }
+    { label: '关于我们', url: 'https://www.zizao.top/fonts/about', openInNewTab: false }
 ];
 
 const footerLinks = [
